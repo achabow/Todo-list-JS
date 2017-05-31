be0db315274946c2c7272cc105ed81af74e040e6
Andrzej Chabowski <Andrzej.Chabowski@lppsa.com>
2017-05-31 15:46:14 +0200

    almost finished add inpute function...

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,1 +1,28 @@
-document.getElementById
+window.onload = function ()
+{
+   document.getElementById('add').addEventListener('click', function(){
+      var value = document.getElementById('item').value; 
+      if (value){
+       addItemTodo(value); 
+      }  else {
+          console.log('STFU !!');  
+      } 
+       
+   });
+    
+function addItemTodo(text){
+    var item = document.createElement('li');
+    item.innerText = text;
+    
+    var buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+    
+    
+    var remove = document.createElement('button');
+    remove.classList.add('remove');
+    
+    var add = document.createElement('button');
+    complete.classList.add('complete ');
+    
+    }
+}
